9610fee7f458fb6603b93e5272a36941e47d335e
solveiaf <solveiaf@student.matnat.uio.no>
2016-11-09 19:06:48 +0100

    Added something

--- Project4/Project4A/main.cpp
+++ Project4/Project4A/main.cpp
@@ -1,12 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <armadillo>
+#include <cmath>
+#include <fstream>
+
 using namespace std;
+using namespace arma;
+
+ofstream ofile;
+
+inline int periodic(int i, int limit,int add) {
+    return (i+limit+add) % (limit);
+}
+
+void initializeLattice(int Nspins, mat &SpinMatrix, double& Energy, double MagneticMoment);
+void Metropolis(int number_of_spins, long &idum, int &E, int &M, double *w, int **spin_matrix);
+void output(int, int, double, vec);
 
 int main()
 {
     return 0;
 }
+
+void initializeLattice(int Nspins, mat &SpinMatrix, int &Energy, int &MagneticMoment);
+{
+    for(int x =0; x<Nspins; x++){
+        for(int y=0; y<Nspins; y++){
+            SpinMatrix(x,y) = 1.0;
+            MagneticMoment += (double) SpinMatrix(x,y);
+
+        }
+    }
+    for(int x=0; x<Nspins; x++){
+        for(int y=o; y<Nspins; y++)
+            Energy -= (double) SpinMatrix(x,y)*
+                    (SpinMatrix(periodic(x,Nspins,-1),y) +
+                     SpinMatrix(x,periodic((y,Nspins, -1)));
+
+}
+}
+
 
 void Metropolis(int number_of_spins, long& idum, int& E, int& M, double *w, int **spin_matrix) {
     for(int x=0; x<number_of_spins; x++) {
@@ -24,4 +58,23 @@ void Metropolis(int number_of_spins, long& idum, int& E, int& M, double *w, int 
             }
         }
     }
+}
+void output(int NSpins, int MCcycles, double temperature, double ExpectationValues){
+    double norm = 1.0/((double) (MCcycles));
+    double E_ExpectationValues = ExpectationValues(0)*norm;
+    double E2_ExpectationValues = ExpectationValues(1)*norm;
+    double M_expectationValues = ExpectationValues(2)*norm;
+    double M2_ExpectationValues = ExpectationValues(3)*norm;
+    double Mavs_Expectationvalues = ExpectationValues(4)*norm;
+
+    //Variansen
+
+    ofile << setiosflags(ios::showpoint  |  ios::uppercase);
+    ofile << setw(15) << setprecision(8) <<  ;
+    ofile << setw(15) << setprecision(8) << ;
+    ofile << setw(15) << setprecision(8) << ;
+    ofile << setw(15) << setprecision(8) << ;
+    ofile << setw(15) << setprecision(8) << ;
+    ofile << setw(15) << setprecision(8) << ;
+
 }
