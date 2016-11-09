92b60699dcc5f23b9985e8ad5894bfd8bb13bff4
Sarah <sarah.rastad@gmail.com>
2016-11-09 18:36:41 +0100

    ja?

--- Project4/Project4A/main.cpp
+++ Project4/Project4A/main.cpp
@@ -1,8 +1,27 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    return 0;
+}
 
-    return a.exec();
+void Metropolis(int number_of_spins, long& idum, int& E, int& M, double *w, int **spin_matrix) {
+    for(int x=0; x<number_of_spins; x++) {
+        for(int y=0; y<number_of_spins; y++) {
+            int ix = (int) (ran1(&idum)*(double)number_of_spins);
+            int iy = (int) (ran1(&idum)*(double)number_of_spins);
+            int dE = 2*spin_matrix[ix][iy]*(spin_matrix[ix][periodic(iy,number_of_spins,-1)] +
+                    spin_matrix[periodic(ix,number_of_spins,-1)][iy] +
+                    spin_matrix[ix][periodic(iy,number_of_spins,1)] +
+                    spin_matrix[periodic(ix,number_of_spins,1)][iy]);
+            if(ran1(&idum)<=w[dE+8]) {
+                spin_matrix[ix][iy] *= -1;
+                M += 2*spin_matrix[ix][iy];
+                E += dE;
+            }
+        }
+    }
 }
