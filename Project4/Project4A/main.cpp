2e7fcba7da2f5687f99468c59f30fa005959ae84
solveiaf <solveiaf@student.matnat.uio.no>
2016-11-10 12:20:21 +0100

    o

--- Project4/Project4A/main.cpp
+++ Project4/Project4A/main.cpp
@@ -19,18 +19,20 @@ inline int periodic(int i, int limit,int add) {
 void initializeLattice(int Nspins, int** &SpinMatrix, int &Energy, int &MagneticMoment);
 void Metropolis(int number_of_spins, long &idum, int &E, int &M, double *w, int **spin_matrix);
 void output(int, int, double, double*);
+double partition_function(double* w);
+double heat_capacity(double* average, int MCcycles, double Z);
 
 int main()
 {
     char *outfilename;
     long idum;
     int **spin_matrix, number_of_spins, mcs, E, M;
-    double w[17], average[5], temp;
+    double w[17], average[5], temp, Z, Cv;
 
     outfilename = "results.txt";
     ofile.open(outfilename);
     number_of_spins = 2;
-    mcs = 100;
+    mcs = 1000;
     temp = 1;
     // spin_matrix = new int*[number_of_spins];
 
@@ -45,6 +47,8 @@ int main()
     for (int de= -8; de <= 8; de+=4) w[de+8] = exp(-de/temp);
     for(int i=0; i<5; i++) average[i] = 0;
 
+    Z = partition_function(w);
+
     initializeLattice(number_of_spins, spin_matrix, E, M);
 
     for(int cycle=1; cycle<=mcs; cycle++) {
@@ -55,6 +59,8 @@ int main()
         average[3] += M*M;
         average[4] += fabs(M);
     }
+    Cv = heat_capacity(average, mcs, Z);
+    cout << "Cv" << Cv << endl;
     output(number_of_spins,mcs,temp,average);
     ofile.close();
     return 0;
@@ -81,6 +87,8 @@ void initializeLattice(int Nspins, int** &SpinMatrix, int &Energy, int &Magnetic
 
 
 void Metropolis(int number_of_spins, long& idum, int& E, int& M, double *w, int **spin_matrix) {
+    int i = 0;
+    int t = 0;
     for(int x=0; x<number_of_spins; x++) {
         for(int y=0; y<number_of_spins; y++) {
             int ix = (int) (ran1(&idum)*(double)number_of_spins);
@@ -93,9 +101,29 @@ void Metropolis(int number_of_spins, long& idum, int& E, int& M, double *w, int 
                 spin_matrix[ix][iy] *= -1;
                 M += 2*spin_matrix[ix][iy];
                 E += dE;
+
+                i++;
             }
         }
     }
+}
+
+double partition_function(double *w){
+    double Z;
+    for(int i = 0; i <100; i++){
+        Z += w[i];
+
+    }
+    return Z;
+
+}
+
+double heat_capacity(double *average, int MCcycles, double Z)
+{
+    double kT2 = 1.0;
+    double norm = 1.0/((double) (MCcycles));
+    double Cv = (1./kT2)*((1/Z)*average[1]*norm - (1/(Z*Z))*average[0]*average[0]*norm*norm);
+    return Cv;
 }
 
 void output(int NSpins, int MCcycles, double temperature, double* ExpectationValues){
